0600e24c3522909aad2800ce157dc0acea5d2673
elloello <74047309+elloello@users.noreply.github.com>
2022-09-10 15:06:10 +0530

    switch to alacritty and change fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -19,3 +19,6 @@ alias vimc="cd ~/.config/nvim && nvim ~/.config/nvim/init.vim"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# THEME PURE #
+set fish_function_path /home/noah/.config/fish/functions/theme-pure/functions/ $fish_function_path
+source /home/noah/.config/fish/functions/theme-pure/conf.d/pure.fish
