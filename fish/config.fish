1bbaac2ef8a4b8cc8cb62729df0fcf6432ecc7a8
noah <74047309+elloello@users.noreply.github.com>
2022-10-05 00:47:28 -0400

    add fedora aliases

--- fish/config.fish
+++ fish/config.fish
@@ -9,19 +9,11 @@ alias cl="clear"
 alias sl="ls"
 alias y="yarn"
 alias sn="shutdown now"
-alias search="nix-env -qa"
-alias list="nix-env -q"
-alias install="nix-env -iA"
-alias garbage="nix-collect-garbage -d"
-alias delete="nix-env -e"
+alias ins="sudo dnf install"
+alias rem="sudo dnf remove"
 alias .="cd"
 alias vimc="cd ~/.config/nvim && nvim ~/.config/nvim/init.vim"
-
-# sh ~/.config/rxfetch/rxfetch
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-# THEME PURE #
-set fish_function_path /home/noah/.config/fish/functions/theme-pure/functions/ $fish_function_path
-source /home/noah/.config/fish/functions/theme-pure/conf.d/pure.fish
